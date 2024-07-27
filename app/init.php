068d19b762ffd1b2ef2299a7eb367fd908ba9ce4
Alexander Kartavtsev <ugo-lok@yandex.ru>
2024-07-27 14:14:48 +0300

    Backend. Подключаем б/д, создаем таблицу users

--- app/init.php
+++ app/init.php
@@ -1,5 +1,6 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/app/functions.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/app/database.php';
 
 if (defined('NEED_AUTH') && NEED_AUTH) {
     checkAuth();
