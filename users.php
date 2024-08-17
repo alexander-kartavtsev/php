6c84654caa9e40cfe3c76842497073e707a3ea8a
Alexander Kartavtsev <ugo-lok@yandex.ru>
2024-08-17 18:52:30 +0300

    FRONTEND. Пагинация

--- users.php
+++ users.php
@@ -1,27 +1,31 @@
 <?php
+
 const NEED_AUTH = true;
 require $_SERVER['DOCUMENT_ROOT'] . '/layout/header.php';
 /** @var User $user */
 ?>
     <div class="page_content users_page">
-      <h1>Данные пользователей</h1>
-      <table class="users_data">
-        <tr>
-          <th class="login">login</th>
-          <th class="name">Имя пользователя</th>
-          <th class="number">Количество посещений</th>
-          <th class="number">Число</th>
-        </tr>
-          <?php foreach (Users::getAll() as $user) { ?>
-        <tr<?= $user->current ? ' class="current"' : '' ?>>
-          <td class="login"><?= $user->getLogin() ?></td>
-          <td class="name"><?= $user->getName() ?></td>
-          <td class="visits_count"><?= $user->getVisits() ?></td>
-          <td class="simple_number"><?= $user->getNumber() ?></td>
-        </tr>
-    <?php } ?>
-      </table>
-
+        <h1>Данные пользователей</h1>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/layout/pagen.php'?>
+        <table class="users_data">
+            <tr>
+                <th class="login">login</th>
+                <th class="name">Имя пользователя</th>
+                <th class="number">Количество посещений</th>
+                <th class="number">Число</th>
+            </tr>
+            <?php
+            foreach (Users::getAll() as $user) { ?>
+                <tr<?= $user->current ? ' class="current"' : '' ?>>
+                    <td class="login"><?= $user->getLogin() ?></td>
+                    <td class="name"><?= $user->getName() ?></td>
+                    <td class="visits_count"><?= $user->getVisits() ?></td>
+                    <td class="simple_number"><?= $user->getNumber() ?></td>
+                </tr>
+            <?php
+            } ?>
+        </table>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/layout/pagen.php'?>
     </div>
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/layout/footer.php';
